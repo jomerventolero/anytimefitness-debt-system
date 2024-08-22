92613d83d1909ea2857537a283f87ce734985a6d
jomerventolero <hiroventolero@gmail.com>
2024-08-22 10:22:17 +0800

    Fix getData Controller

--- controllers/GetDashboardData.ts
+++ controllers/GetDashboardData.ts
@@ -1,9 +1,17 @@
 import { pb } from "@/api/auth/pocketbase";
 
 const getDashboardData = async () => {
-  const profileID = pb.authStore.model?.id;
-  const record = await pb.collection('getDashboardData').getOne(profileID);
-  return record
+  pb.autoCancellation(false);
+  try {
+    const profileID = pb.authStore.model?.id;
+    const record = await pb.collection('getAdminProfileData').getOne(profileID);
+    const gym_id = record?.gym_branch
+    const gym = await pb.collection('branch').getOne(gym_id)
+    console.log(gym.name)
+    return { record, gym }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default getDashboardData
